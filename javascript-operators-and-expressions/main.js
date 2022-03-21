f29a7ad90dc5d862cfbe03ebe48c4e10515a3070
Michael Suh <michaelsuh9901@gmail.com>
2022-03-21 19:32:03 +0000

    javascript-operators-and-expressions - Michael Suh

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,37 @@
+var width = 2;
+var height = 5;
+var area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 3;
+var payment = 30;
+var change = payment - bill;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 2;
+var midterm = 3;
+var final = 4;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Michael';
+var lastName = 'Suh';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 5;
+var isAcidic = (pH < 7);
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic', typeof isAcidic);
+
+var headCount = 10;
+var isSparta = (headCount === 300);
+console.log('value of isSparta:', isSparta);
+
+var motto = fullName + ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
