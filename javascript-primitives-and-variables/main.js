044b28d354dda2a3d7d3cf8d655c3d47788a5df6
Michael Suh <michaelsuh9901@gmail.com>
2022-03-21 17:27:33 +0000

    javascript-primitives-and-variables - Michael Suh

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,21 @@
+var fullName = 'Michael Suh';
+console.log('value of fullName:', fullName);
+
+var isCool = true;
+Boolean(isCool);
+console.log('value of isCool:', isCool);
+
+var totalPets = '0';
+console.log('value of total pets:', totalPets);
+
+console.log('typeof fullName', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+var unknown = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+var never;
+console.log('value of never:', never);
+console.log('typeof never', typeof never);
