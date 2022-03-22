51a2719db43e9afb0c83cb208a64f7a94fe2678a
Michael Suh <michaelsuh9901@gmail.com>
2022-03-22 22:12:19 +0000

    made changes to data model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,71 +1,72 @@
 var orderHistory = [
   {
-    firstOrder:
-    {
-      orderNumber: '114-3941689-8772232',
-      orderPlaced: 'August 4, 2020',
-      total: '$34.00',
-      shipTo: 'JS Masher',
-      delivered: 'Delivered Aug 8, 2020',
-      title: 'JavaScript for impatient programmers',
-      author: 'Rauschmayer, Dr. Axel',
-      returnWindow: 'Return window closed on Sep 7, 2020',
-      price: '$31.55',
-      productReview: false
-    },
-    secondOrder:
-    {
-      orderNumber: '113-9984268-1280257',
-      orderPlaced: 'July 19, 2020',
-      total: '$44.53',
-      shipTo: 'JS Masher',
-      delivered: 'Delivered Jul 20, 2020',
-      title: 'The Timeless Way of Building',
-      author: 'Alexander, Christopher',
-      returnWindow: 'Return window closed on Aug 19, 2020',
-      price: '$41.33',
-      productReview: false
-    },
-    thirdOrder:
-    {
-      orderNumber: '114-2875557-9059409',
-      orderPlaced: 'July 4, 2020',
-      total: '$17.22',
-      shipTo: 'JS Masher',
-      delivered: 'Delivered Jul 7, 2020',
-      title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gametube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
-      author: null,
-      returnWindow: 'Return window closed on Aug 5, 2020',
-      price: '$15.98',
-      productReview: false
-    },
-    fourthOrder:
-    [
-      'itemOne',
+    orderNumber: '114-3941689-8772232',
+    orderPlaced: 'August 4, 2020',
+    total: '$34.00',
+    shipTo: 'JS Masher',
+    delivered: 'Delivered Aug 8, 2020',
+    items: [
       {
-        orderNumber: '113-2883177-2648248',
-        orderPlaced: 'July 3, 2020',
-        total: '$138.93',
-        shipTo: 'JS Masher',
-        delivered: 'Delivered Jul 5 2020',
-        title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        type: 'book',
+        title: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        returnWindow: 'Return window closed on Sep 7, 2020',
+        price: 31.55
+      }
+    ]
+  },
+  {
+    orderNumber: '113-9984268-1280257',
+    orderPlaced: 'July 19, 2020',
+    total: '$44.53',
+    shipTo: 'JS Masher',
+    delivered: 'Delivered Jul 20, 2020',
+    items: [
+      {
+        type: 'book',
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnWindow: 'Return window closed on Aug 19, 2020',
+        price: 41.33
+      }
+    ]
+  },
+  {
+    orderNumber: '114-2875557-9059409',
+    orderPlaced: 'July 4, 2020',
+    total: '$17.22',
+    shipTo: 'JS Masher',
+    delivered: 'Delivered Jul 7, 2020',
+    items: [
+      {
+        type: 'item',
+        name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gametube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
         author: null,
         returnWindow: 'Return window closed on Aug 5, 2020',
-        price: '$94.95',
-        productReview: false
-      },
-      'itemTwo',
+        price: 15.98
+      }
+    ]
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    orderPlaced: 'July 3, 2020',
+    total: '$138.93',
+    shipTo: 'JS Masher',
+    delivered: 'Delivered Jul 5 2020',
+    items: [
       {
-        orderNumber: '113-2883177-2648248',
-        orderPlaced: 'July 3, 2020',
-        total: '$138.93',
-        shipTo: 'JS Masher',
-        delivered: 'Delivered Jul 5 2020',
+        type: 'item',
+        name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        author: null,
+        returnWindow: 'Return window closed on Aug 5, 2020',
+        price: 94.95
+      },
+      {
+        type: 'book',
         title: 'The Art of Sql',
         author: 'Faroult, Stephane',
         returnWindow: 'Return window closed on Aug 4, 2020',
-        price: '$33.99',
-        productReview: false
+        price: 33.99
       }
     ]
   }
