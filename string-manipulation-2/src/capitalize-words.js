e6b38ebb23141a7456477abe2f3339fb43afabb4
Michael Suh <michaelsuh9901@gmail.com>
2022-03-29 00:07:29 +0000

    Still in progress!

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -15,3 +15,11 @@ function capitalizeWords(string) {
   }
   return newstr;
 }
+
+// declare an empty string to the var newstr
+// declare a var with a boolean value
+// use a for loop to loop through the each letter of a string
+// use a conditional to see if capitalization is needed.
+// when capitalization is needed, use toUpperCase to capitalize the letter
+// when capitalization is not needed, use toLowerCase to lowercase the letter
+// when there is an empty space, set boolean to true since the next character needs to be capitalized.
--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,16 @@
 /* exported numVowels */
+function numVowels(string) {
+  var numberOfVowels = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (string[i].toUpperCase() === 'A' || string[i].toUpperCase() === 'E' || string[i].toUpperCase() === 'I' || string[i].toUpperCase() === 'O' || string[i].toUpperCase() === 'U') {
+      numberOfVowels += 1;
+    }
+  }
+  return numberOfVowels;
+}
+
+// Assign value 0 to var numberOfVowels
+// use a for loop to loop through each characters in a string
+// use .toUpperCase() to check if each character is equal to vowels
+// add 1 to var numberOfVowels each time there is a vowel present
+// return numberOfVowels
