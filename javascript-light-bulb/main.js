6b9135978ab0bb076f2776a557446c215da97764
Michael Suh <michaelsuh9901@gmail.com>
2022-03-29 20:23:10 +0000

    used boolean value

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,13 +1,16 @@
 var $centerButton = document.querySelector('.center-button');
 var $backgroundColor = document.querySelector('.background-color');
+var $lightOnOrOff = true;
 
 function clickButton(event) {
-  if ($backgroundColor.className === 'background-color dark') {
-    $centerButton.className = 'center-button bright';
-    $backgroundColor.className = 'background-color bright';
-  } else {
+  if ($lightOnOrOff === true) {
     $centerButton.className = 'center-button dark';
     $backgroundColor.className = 'background-color dark';
+    $lightOnOrOff = false;
+  } else if ($lightOnOrOff === false) {
+    $centerButton.className = 'center-button bright';
+    $backgroundColor.className = 'background-color bright';
+    $lightOnOrOff = true;
   }
 }
 
