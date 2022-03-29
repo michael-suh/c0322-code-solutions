05d26dd5e17df3f5b53bf6489415554e44cf50b8
Michael Suh <michaelsuh9901@gmail.com>
2022-03-29 18:08:56 +0000

    made changes to js file

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,15 +1,13 @@
-var numberOfClicks = 0;
 var $centerButton = document.querySelector('.center-button');
 var $backgroundColor = document.querySelector('.background-color');
 
 function clickButton(event) {
-  numberOfClicks++;
-  if (numberOfClicks % 2 === 0) {
-    $centerButton.className = 'center-button dark';
-    $backgroundColor.className = 'background-color dark';
-  } else {
+  if ($backgroundColor.className === 'background-color dark') {
     $centerButton.className = 'center-button bright';
     $backgroundColor.className = 'background-color bright';
+  } else {
+    $centerButton.className = 'center-button dark';
+    $backgroundColor.className = 'background-color dark';
   }
 }
 
