17ca3b554b14f4180a0b4942fda4c9f2881570c0
Michael Suh <michaelsuh9901@gmail.com>
2022-03-30 00:48:58 +0000

    still in progress!

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,1 +1,16 @@
 /* exported firstChars */
+function firstChars(length, string) {
+  var result = '';
+  for (var i = 0; i < length; i++) {
+    if (string[i]) {
+      result += string[i];
+    }
+  }
+  return result;
+}
+
+// create an empty string for var result
+// use a for loop to loop through the length of a string
+// for condition in the for loop, run it until i is less than the length (input)
+// use if conditional statement to add the empty string and string[i] together
+// return var result
