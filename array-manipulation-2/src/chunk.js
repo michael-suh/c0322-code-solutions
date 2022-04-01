ac540a00b267eb378c59af8107e4029e79430638
Michael Suh <michaelsuh9901@gmail.com>
2022-04-01 21:31:14 +0000

    still in progress!

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -8,10 +8,18 @@ function chunk(array, size) {
   for (var i = 0; i < array.length; i++) {
     if (newArray.length === size) {
       result.push(newArray);
-      newArray = [];
     }
     newArray.push(array[i]);
   }
   result.push(newArray);
   return result;
 }
+
+// declare two empty arrays with two different var
+// use if statement to see if array.length = 0 and if it is, return an empty array
+// use a for loop to loop through each items in the array
+// use if statement to set the newarray.length equal to size
+// if it equals to sie, push new array
+// if not, push array[i] into newarray
+// push new array into the result array
+// return output
