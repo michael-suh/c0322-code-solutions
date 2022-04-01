0c3641245020ad69ad6dd58f3e9ab2508bcb6750
Michael Suh <michaelsuh9901@gmail.com>
2022-04-01 00:57:26 +0000

    still in progress!

--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,1 +1,16 @@
 /* exported includes */
+function includes(array, value) {
+  var result = false;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return true;
+    }
+  }
+  return result;
+}
+
+// assign boolean value false to var result
+// use for loop to loop through each items in the array
+// use if statement to see if value is present at array[i]
+// if present, return true
+// if not, return var result.
--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,1 +1,10 @@
 /* exported takeRight */
+function takeRight(array, count) {
+  var result = [];
+  for (var i = array.length - count; i < array.length; i++) {
+    result.push(array[i]);
+  } if (array.length < count) {
+    result = array;
+  }
+  return result;
+}
