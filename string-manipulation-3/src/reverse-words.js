496be46c7ed2c7ff20a231e1f5ffa5b798982240
Michael Suh <michaelsuh9901@gmail.com>
2022-04-12 00:17:47 +0000

    edited the amount of loops for reverseWords

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,8 +1,13 @@
 /* exported reverseWords */
 function reverseWords(string) {
-  return string.split('').reverse().join('').split(' ').reverse().join(' ');
+  var result = '';
+  for (var i = string.length - 1; i >= 0; i--) {
+    result += string[i];
+  }
+  return result.split(' ').reverse('').join(' ');
 }
-// split each word from string
-// reverse every word
-// join them together
-// repeat the first three steps except make a space for split and join
+
+// set var result equal to an empty string
+// use reverse for loop to loop through string and reverse initialization, conditions, and final expression.
+// set result equal to result + string[i]
+// use split, reverse and join method to return result string
