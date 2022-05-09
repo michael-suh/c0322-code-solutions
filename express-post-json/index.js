9024d683c7ed72d889f5000778d57e6505b72da4
Michael Suh <michaelsuh9901@gmail.com>
2022-05-09 17:00:07 +0000

    completed express-post-json exercise

---
+++ express-post-json/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+let nextId = 1;
+const grades = {};
+
+app.get('/api/grades', (req, res) => {
+  const gradesArray = [];
+  for (const key in grades) {
+    gradesArray.push(grades[key]);
+  }
+  res.json(gradesArray);
+});
+
+const newGrade = express.json();
+app.use(newGrade);
+
+app.post('/api/grades', (req, res) => {
+  req.body.id = nextId;
+  grades[nextId] = req.body;
+  nextId++;
+  res.status(201).send(req.body);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express server listening on port 3000');
+});
